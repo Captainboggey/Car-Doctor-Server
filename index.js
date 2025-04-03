c7728bb83a5135fa13a5045d33dad5069afa3179
captainboogey <anik35-700@diu.edu.bd>
2025-04-03 19:20:19 +0600

    final commit

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser")
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173','cars-doctor-cc5d1.web.app','cars-doctor-cc5d1.firebaseapp.com'],
+  credentials:true
+}))
 app.use(express.json());
-require('dotenv').config()
+require('dotenv').config();
+app.use(cookieParser());
 
 
 
@@ -22,14 +28,54 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+// middlewares
+const logger = (req,res,next)=>{
+  console.log(req.url);
+  next()
+}
+const verifyToken = (req,res,next)=>{
+  const token = req?.cookies?.token
+  if(!token){
+   return res.status(401).send({status:'UnAuthorized Access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN ,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({status:"UnAuthorized Access"})
+    }
+    req.user = decoded;
+    next()
+  })
+}
+const cookieOption = {
+  httpOnly:true,
+  secure: process.env.NODE_ENV === "production"? "true": false,
+  sameSite: process.env.NODE_ENV === "production"? "none" : "strict"
+}
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const serviceCollection = client.db('carDoctor') .collection('services');
     const bookingCollection = client.db('carDoctor') .collection("bookings")
+  
 
+    // jwt
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN ,{expiresIn:'1h'})
+      res
+      .cookie('token',token ,cookieOption)
+      .send({status:'success'})
+    })
+
+    app.post('/logout',async(req,res)=>{
+      
+      res.clearCookie('token',{...cookieOption, maxAge: 0}).send({status:'cookie removed'})
+    })
+
+
+    // services
     app.get("/services", async(req,res)=>{
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
@@ -47,7 +93,10 @@ async function run() {
     })
 // bookings
 
-app.get("/bookings", async(req,res)=>{
+app.get("/bookings",logger, verifyToken, async(req,res)=>{
+  if(req.query.email !== req.user.email){
+    return res.status(403).send({status:'forbidden access'})
+  }
     let query ={};
     if(req.query?.email){
         query={email: req.query.email}
@@ -88,8 +137,8 @@ app.delete("/bookings/:id",async(req,res)=>{
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
